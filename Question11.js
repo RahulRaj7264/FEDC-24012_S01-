1a15f2661941c6d26c14782844d43cad0c331c65
rahulrajsingh113@gmail.com <Rahul@7462>
2024-12-19 00:51:59 +0530

    I have completed

---
+++ Question11.js
@@ -0,0 +1,6 @@
+let to_DoList =["Wake up","Brush teeth","Have breakfast"];
+console.log("Initial task:",to_DoList);
+to_DoList.push("Go for a walk");
+console.log("Add a new task:",to_DoList);
+to_DoList.pop("Go for a walk");
+console.log("Remove the last task:",to_DoList);
